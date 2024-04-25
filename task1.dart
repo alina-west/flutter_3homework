44daf5462c0b53e169b736ca9f3a1e606791bb6d
alina-west <144222854+alina-west@users.noreply.github.com>
2024-04-25 08:42:45 +0500

    added task1.dart

--- task1.dart
+++ task1.dart
@@ -5,44 +5,44 @@ void main() {
   int number = int.parse(stdin.readLineSync()!);
 
   // Проверка на 4 или 7
-  bool contains4or7 = false;
-  int tempNumber = number;
-  while (tempNumber > 0) {
-    int digit = tempNumber % 10;
+  bool jetiMentort = false;
+  int sandarT = number;
+  while (sandarT > 0) {
+    int digit = sandarT % 10;
     if (digit == 4 || digit == 7) {
-      contains4or7 = true;
+      jetiMentort = true;
       break;
     }
-    tempNumber ~/= 10;
+    sandarT ~/= 10;
   }
 
   // Проверка на 3, 6 или 9
-  bool contains3or6or9 = false;
-  tempNumber = number;
-  while (tempNumber > 0) {
-    int digit = tempNumber % 10;
+  bool ushIliTogyzIliAlty = false;
+  sandarT = number;
+  while (sandarT > 0) {
+    int digit = sandarT % 10;
     if (digit == 3 || digit == 6 || digit == 9) {
-      contains3or6or9 = true;
+      ushIliTogyzIliAlty = true;
       break;
     }
-    tempNumber ~/= 10;
+    sandarT ~/= 10;
   }
 
   // Проверка на а
   stdout.write("Введите цифру а: ");
   int a = int.parse(stdin.readLineSync()!);
-  bool containsA = false;
-  tempNumber = number;
-  while (tempNumber > 0) {
-    int digit = tempNumber % 10;
+  bool berilgenA = false;
+  sandarT = number;
+  while (sandarT > 0) {
+    int digit = sandarT % 10;
     if (digit == a) {
-      containsA = true;
+      berilgenA = true;
       break;
     }
-    tempNumber ~/= 10;
+    sandarT ~/= 10;
   }
 
-  print("Входят ли в число цифры 4 или 7: $contains4or7");
-  print("Входят ли в число цифры 3, 6 или 9: $contains3or6or9");
-  print("Входит ли в число цифра $a: $containsA");
+  print("Входят ли в число цифры 4 или 7: $jetiMentort");
+  print("Входят ли в число цифры 3, 6 или 9: $ushIliTogyzIliAlty");
+  print("Входит ли в число цифра $a: $berilgenA");
 }
